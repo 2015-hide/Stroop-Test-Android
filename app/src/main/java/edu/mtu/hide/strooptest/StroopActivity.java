e45cdd0d37b44f9fdd333adaec05bf7ee0fee3a8
njculber <njculber@mtu.edu>
2015-04-05 17:25:51 -0400

    Added comments and fixed the packages

--- app/src/main/java/edu/mtu/hide/strooptest/StroopActivity.java
+++ app/src/main/java/edu/mtu/hide/strooptest/StroopActivity.java
@@ -1,4 +1,8 @@
-package edu.mtu.humaneinterfacedesignenterprise.strooptest;
+/**
+ * @author Eric Kosovec, Stephen Radachy, Neil Culbertson, Brian Berg, Kyle Timmerman.
+ * March 28, 2015
+ */
+package edu.mtu.hide.strooptest;
 
 import android.content.Intent;
 import android.graphics.Color;
@@ -7,17 +11,21 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ListView;
 
+import edu.mtu.humaneinterfacedesignenterprise.strooptest.R;
 
+/**
+ * The stroop activity, which is the stroop test itself.
+ */
 public class StroopActivity extends ActionBarActivity {
 
     @Override
+    /**
+     * Creates the game with the specified button that was clicked
+     * by the user.
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stroop);
@@ -26,9 +34,11 @@ public class StroopActivity extends ActionBarActivity {
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.rgb(66, 169, 61)));
         actionBar.setTitle(Html.fromHtml("<font color='#FFFFFF'>" + getString(R.string.app_name) + "</font>"));
 
+        // create buttons 40 and 60 km/hr
         Button button40 = (Button) findViewById(R.id.km40button);
         Button button60 = (Button) findViewById(R.id.km60button);
 
+        // creates button listener for when the user clicks the 40km test.
         button40.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,25 +46,30 @@ public class StroopActivity extends ActionBarActivity {
             }
         });
 
+        // creates button listener for when the user clicks the 60km test.
         button60.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 to60KMTest();
             }
         });
-    }
+    } // end of onCreate method
 
+    /**
+     * Begins the stroop test with 40 km/hr settings.
+     */
     public void to40KMTest() {
         Intent intent = new Intent(this, StroopTestsActivity.class);
         intent.putExtra(StroopActivity.class.getName(), true);
         startActivity(intent);
     }
 
+    /**
+     * Begins the stroop test with 40 km/hr settings.
+     */
     public void to60KMTest() {
         Intent intent = new Intent(this, StroopTestsActivity.class);
         intent.putExtra(StroopActivity.class.getName(), false);
         startActivity(intent);
-    }
-
-
-}
+    } // end of to60KMTest
+} // end of class
--- app/src/main/java/edu/mtu/hide/strooptest/StroopTestsActivity.java
+++ app/src/main/java/edu/mtu/hide/strooptest/StroopTestsActivity.java
@@ -1,4 +1,8 @@
-package edu.mtu.humaneinterfacedesignenterprise.strooptest;
+/**
+ * @author Eric Kosovec, Stephen Radachy, Neil Culbertson, Brian Berg, Kyle Timmerman.
+ * March 28, 2015
+ */
+package edu.mtu.hide.strooptest;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -11,14 +15,26 @@ import android.content.Context;
 
 import java.util.ArrayList;
 
+import edu.mtu.humaneinterfacedesignenterprise.strooptest.R;
 
+/**
+ *  This class creates a menu for the user to initiate the 40 or 60
+ *  km/hr stroop test activity.
+ */
 public class StroopTestsActivity extends ActionBarActivity {
 
+    // initialize Arraylist for the stroop cards
     private ArrayList<Integer> cards;
+    // the transparent card counter to keep trakc number of transparent cards
     private int transparentCard;
+    // the imageview for the menu
     private ImageView stroopView;
     final Context context = this;
     @Override
+    /**
+     * Creates menu for the stroop test by displaying two buttons marked
+     * 40 km/hr and 60 km/hr and the hide logo.
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stroop_tests);
@@ -51,12 +67,16 @@ public class StroopTestsActivity extends ActionBarActivity {
         }
     }
 
+    /**
+     * The 40 km/hr specified timings version for the stroop test.
+     */
     private void test40KM() {
         final Handler handler = new Handler();
 
         Runnable runnable = new Runnable() {
             int i = 0;
 
+            //
             public void run() {
                 if (i == 25) {
                     // goes back to main menu
@@ -65,25 +85,31 @@ public class StroopTestsActivity extends ActionBarActivity {
                 }
                 
                 stroopView.setImageResource(cards.get(i));
-                
+
+                // timing variable
                 int timing;
 
+                // Up until card 1, will wait 35 seconds to display first card.
                 if (i == 0) {
                     timing = 35000;
                 }
 
+                // At the 12th card, there will be a timing delay of 15 seconds.
                 else if (i == 12) {
                     timing = 15000;
                 }
 
+                // the even cards with a stop of 11seconds.
                 else if ((i & 1) == 0) { // even - sets up time for next stroop card
                     timing = 11000;
                 }
 
+                // Every other card will appear every three seconds.
                 else { // odd
                     timing = 3000;
                 }
 
+                // increment card position
                 i++;
 
                 handler.postDelayed(this, timing); // Set up next card to appear at the determined time.
@@ -93,6 +119,9 @@ public class StroopTestsActivity extends ActionBarActivity {
         handler.postDelayed(runnable, 0); // Initial delay before the start - 0 ms.
     }
 
+    /**
+     * The 60 km/hr timings version for the stroop test.
+     */
     private void test60KM() {
         final Handler handler = new Handler();
 
@@ -106,25 +135,31 @@ public class StroopTestsActivity extends ActionBarActivity {
                 }
                 
                 stroopView.setImageResource(cards.get(i));
-                
+
+                // timing variable.
                 int timing;
 
+                // Up until card 1, will wait 23.5 seconds to display first card.
                 if (i == 0) {
                     timing = 23500;
                 }
 
+                // At the 12th card, there will be a timing delay of 7.5 seconds.
                 else if (i == 12) {
                     timing = 7500;
                 }
 
+                // the even cards with a stop of six seconds.
                 else if ((i & 1) == 0) { // even
                     timing = 6000;
                 }
 
+                // Every other card will appear every three seconds.
                 else {
                     timing = 3000;
                 }
 
+                // increment card position
                 i++;
 
                 handler.postDelayed(this, timing); // Set up next card to appear at the determined time.
@@ -134,9 +169,14 @@ public class StroopTestsActivity extends ActionBarActivity {
         handler.postDelayed(runnable, 0); // Initial delay before the start - 0 ms.
     }
 
+    /**
+     * Generates card list consisting of twelve different cards for the
+     * Stroop activity. YEAH!
+     */
     private void generateCardsList() {
         cards = new ArrayList<Integer>();
 
+        // add cards to arraylist.
         cards.add(transparentCard);
         cards.add(R.drawable.one);
         cards.add(transparentCard);
